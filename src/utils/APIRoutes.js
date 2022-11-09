5975319b756f726404946970c06556836504054c
Ayush Gupta <ayugupcse@gmail.com>
2022-11-09 23:52:30 +0530

    added build

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
- export const host = "http://localhost:5000"
+ export const host = "https://chatt-app-backend.herokuapp.com"
  export const registerRoute = `${host}/api/user/register`
  export const loginRoute = `${host}/api/user/login`
  export const logoutRoute = `${host}/api/user/logout`
